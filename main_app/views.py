f9ca42d11a5ece7178385d7bd23a6a80929d000f
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2023-01-13 17:05:35 -0500

    wip: adding data

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-# Add the Dog class & list and view function below the imports
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Walter', 'Domestic Short Hair', 'My son.', 8),
-  Dog('Jack', 'Domestic Short Hair', 'Walter\'s bro.', 8),
-  Dog('Clive', 'Tabby', 'Good guy. Sad story. Feels like you\'re petting a bag of bones.', 4)
-]
+from .models import Dog
 
 # Create your views here.
 from django.http import HttpResponse
@@ -25,4 +12,5 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.order_by('name')
     return render(request, 'dogs/index.html', { 'dogs': dogs })
