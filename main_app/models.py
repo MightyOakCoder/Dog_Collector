dbf878b06d88f11a7d97f00b7c18595ec2698037
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2023-01-14 11:46:17 -0500

    finishing pts 1&2

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     favorite_activity = models.TextField(max_length=250)
-    age = models.IntegerField()
+    famous_for = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
