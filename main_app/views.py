57ed3379b66d7195de3db5f7a0887208c91bfa03
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2023-01-17 16:45:26 -0500

    adding updates from lab 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -18,3 +19,16 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'favorite_activity', 'famous_for']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
